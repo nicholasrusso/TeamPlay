99078525781ff549b08c71b4c19f45e290037d98
Spencer Sprowls <spencer.sprowls@gmail.com>
2016-05-05 16:11:12 -0700

    550708 return a method in panel

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -2,63 +2,95 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
 import javax.swing.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.BorderLayout;
 
 public class MainMenu {
     private JFrame frame;
     private JPanel headerPanel, menuPanel, menuButtonPanel, profilePanel, settingsPanel, createTournamentPanel, tournamentPanel, rulesPanel;
     private JButton rulesButton, tournamentButton, createTournamentButton, editProfileButton, settingsButton, backButton; 
+    private JLabel logoLabel, background;
+    private Image img;
 
     public void createMenu() {
+
         frame = new JFrame();
-        
         menuPanel = new JPanel();
         menuButtonPanel = new JPanel();
         headerPanel = new JPanel();
 
+        //try {
+        //    
+        //    BufferedImage myPicture = ImageIO.read(new File("C:/Users/Owner/Desktop/logo.png"));
+        //    logoLabel = new JLabel(new ImageIcon(myPicture));
+        //    headerPanel.setLayout(null);
+        //    headerPanel.add(logoLabel);
+
+        //    logoLabel.setBounds(20, 20, 220, 160);
+        //}
+        //catch (Exception e) {
+        //}
+
         frame.setTitle("TeamPlay");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,600);
-        frame.setResizable(false);
+        frame.setResizable(false);                  
 
-        Dimension d = new Dimension(360,40);
+        Dimension d = new Dimension(420,30);
         tournamentButton = new JButton("Join Tournament");
         tournamentButton.addActionListener(new tournamentButtonListener());
         tournamentButton.setPreferredSize(d);
-        
+        tournamentButton.setBorder(BorderFactory.createLineBorder(Color.black));
+        tournamentButton.setFont(tournamentButton.getFont().deriveFont(Font.BOLD));
+
         createTournamentButton = new JButton("Create Tournament");
         createTournamentButton.addActionListener(new createTournamentButtonListener());
         createTournamentButton.setPreferredSize(d);
+        createTournamentButton.setBorder(BorderFactory.createLineBorder(Color.black));
+        createTournamentButton.setFont(createTournamentButton.getFont().deriveFont(Font.BOLD));
 
         editProfileButton = new JButton("Edit Profile");
         editProfileButton.addActionListener(new editProfileButtonListener());
-        editProfileButton.setPreferredSize(d);   
-        
+        editProfileButton.setPreferredSize(d);
+        editProfileButton.setBorder(BorderFactory.createLineBorder(Color.black));
+        editProfileButton.setFont(editProfileButton.getFont().deriveFont(Font.BOLD));
+
         rulesButton = new JButton("Rules");
         rulesButton.addActionListener(new rulesButtonListener());
         rulesButton.setPreferredSize(d);     
-        
+        rulesButton.setBorder(BorderFactory.createLineBorder(Color.black));
+        rulesButton.setFont(rulesButton.getFont().deriveFont(Font.BOLD));
+
         settingsButton = new JButton("Settings");
         settingsButton.addActionListener(new settingsButtonListener());
-        settingsButton.setPreferredSize(d); 
-        
+        settingsButton.setPreferredSize(d);
+        settingsButton.setBorder(BorderFactory.createLineBorder(Color.black));
+        settingsButton.setFont(settingsButton.getFont().deriveFont(Font.BOLD));
 
-               
         menuButtonPanel.add(tournamentButton);
         menuButtonPanel.add(createTournamentButton);
         menuButtonPanel.add(rulesButton);
         menuButtonPanel.add(editProfileButton);
         menuButtonPanel.add(settingsButton);
-        menuButtonPanel.setPreferredSize(new Dimension(360, 600));        
-        
-        headerPanel.setPreferredSize(new Dimension(800, 200));
-        
+        menuButtonPanel.setPreferredSize(new Dimension(420, 600));        
+
+        headerPanel.setPreferredSize(new Dimension(800, 260));
+
         menuPanel.add(headerPanel);
         menuPanel.add(menuButtonPanel);        
-        
-        
+        menuButtonPanel.repaint();
         menuPanel.setVisible(true);
-        frame.getContentPane().add(menuPanel);
-        //frame.pack();
+
+        frame.getContentPane().add(menuPanel);   
+
         frame.setVisible(true);
     }
 
@@ -66,20 +98,15 @@ public class MainMenu {
     {
         public void actionPerformed(ActionEvent ae) 
         {
-            frame.getContentPane().removeAll();            
-            tournamentPanel = new JPanel();
-            tournamentPanel.add(new JLabel("Join Tournament Page"));
+            frame.getContentPane().removeAll();       
+            
 
-            backButton = new JButton("Back");
-            backButton.addActionListener(new backButtonListener());
-            tournamentPanel.add(backButton);
-
-            frame.getContentPane().add(tournamentPanel);
+            frame.getContentPane().add(joinTournamentPanel());
             frame.getContentPane().validate();
             frame.getContentPane().repaint();
         }
     }
-    
+
     class createTournamentButtonListener implements ActionListener
     {
         public void actionPerformed(ActionEvent ae) 
@@ -115,7 +142,7 @@ public class MainMenu {
             frame.getContentPane().repaint();
         }
     }
-    
+
     class settingsButtonListener implements ActionListener
     {
         public void actionPerformed(ActionEvent ae) 
@@ -133,7 +160,7 @@ public class MainMenu {
             frame.getContentPane().repaint();
         }
     }
-    
+
     class rulesButtonListener implements ActionListener
     {
         public void actionPerformed(ActionEvent ae) 
@@ -152,7 +179,6 @@ public class MainMenu {
         }
     }
 
-
     class backButtonListener implements ActionListener
     {
         public void actionPerformed(ActionEvent ae) 
@@ -162,6 +188,18 @@ public class MainMenu {
             frame.getContentPane().validate();
             frame.getContentPane().repaint();
         }
+    }
+
+    public JPanel joinTournamentPanel() {
+
+        tournamentPanel = new JPanel();
+        tournamentPanel.add(new JLabel("Join Tournament Page"));
+
+        backButton = new JButton("Back");
+        backButton.addActionListener(new backButtonListener());
+        tournamentPanel.add(backButton);
+
+        return tournamentPanel;
     }
 
     public static void main(String[] args) {
