bddd979cc1024582e8b995f2c1c99db034c34f04
Michael Haskell (mikehas) <mikehas@gmail.com>
2016-05-05 19:20:44 -0700

    537552 - Added database connection factory.

---
+++ src/db/DBFactory.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBFactory {
+
+	public DBFactory() {
+		
+	}
+	public static Connection getDBConnection() {
+		Connection db = null;
+
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      db = DriverManager.getConnection("jdbc:sqlite:data/test.db");
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Opened database successfully");
+	    return db;
+	}
+
+}
