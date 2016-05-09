327ba7ff50891d281f1b907caea50727b4e93c28
Chauncey Neyman <cjneyman@gmail.com>
2016-05-08 17:58:37 -0700

    541285 - Create Tournament menu finalized, must still be synched with tournament class

--- src/CreateTournament.java
+++ src/CreateTournament.java
@@ -1,18 +1,30 @@
+//import src.tournament;
+
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 public class CreateTournament {
    public String tournamentName;
-   public ArrayList<SoccerPlayer>() playerPool;
-   public int numberUsers;
+   public ArrayList<SoccerPlayer> playerPool;
+   public int numberUsers = 2;
 
-   private ArrayList<ProfessionalTeam> allTeams;
+   //private ArrayList<ProfessionalTeam> allTeams;
    private JFrame frame;
    private JPanel teamsPanel;
+
+   /*
+   public Tournament returnTournament() {
+      Tournament t = new Tournament(tournamentName, numberUsers, playerPool);
+      return t;
+   }
+   */
 
    void createTournamentMenu() {
       // Initialize Frame
@@ -26,7 +38,7 @@ public class CreateTournament {
 
       // Upload All Teams w/ Rosters (currently from CSV)
       UploadTeamsByCSV ut = new UploadTeamsByCSV();
-      allTeams = ut.upload();
+      //allTeams = ut.upload();
 
       // JCheckBoxes (one for each team that has a teamName)
       String[] teamNames = {"Arsenal", "Atletico Madrid", "Bayern Munich", 
@@ -46,10 +58,12 @@ public class CreateTournament {
                if (e.getStateChange() == ItemEvent.SELECTED) {
                   System.out.println("Selected " + teamName);
                   // Add all players on that team to pool
+                  // (pool is ArrayList<SoccerPlayer> playerPool)
                }
                else {
                   System.out.println("Deselected " + teamName);
                   // Remove all players from that team to pool
+                  // (pool is ArrayList<SoccerPlayer> playerPool)
                }
             }
          });
@@ -61,14 +75,41 @@ public class CreateTournament {
       frame.getContentPane().add(teamsPanel);
       teamsPanel.setPreferredSize(new Dimension(400, 300));
 
-      // Add Top Label to Panel
-      JLabel selectTeams = new JLabel("Select Teams: ");
-      selectTeams.setBounds(100, 20, 300, 40);
-      teamsPanel.add(selectTeams);
+      // Add Tournament Name via JTextField
+      JLabel tournamentNameLabel = new JLabel("Tournament Name: ");
+      tournamentNameLabel.setBounds(100, 20, 300, 40);
+      teamsPanel.add(tournamentNameLabel);
 
-      // Add Checkboxes to Panel
+      JTextField tournamentNameField = new JTextField();
+      tournamentNameField.setBounds(100, 50, 300, 40);
+      teamsPanel.add(tournamentNameField);
+      tournamentName = tournamentNameField.getText();
+
+      // Add Max # Users via JSlider
+      JLabel maxUsersLabel = new JLabel("Maximum number of players: ");
+      maxUsersLabel.setBounds( 100, 80, 300, 40);
+      teamsPanel.add(maxUsersLabel);
+
+      JSlider maxNumUsers = new JSlider(0, 20, 2);
+      maxNumUsers.setMajorTickSpacing(5);
+      maxNumUsers.setMinorTickSpacing(1);
+      maxNumUsers.setPaintTicks(true);
+      maxNumUsers.setPaintLabels(true);
+      maxNumUsers.setBounds(100, 110, 300, 40);
+      maxNumUsers.addChangeListener(new ChangeListener() {
+         public void stateChanged(ChangeEvent e) {
+            numberUsers = ((JSlider)e.getSource()).getValue();
+            System.out.println("numberUsers: " + numberUsers);
+         }
+      });
+      teamsPanel.add(maxNumUsers);
+
+      // Add Team Selection via JCheckBoxes
+      JLabel selectTeamsLabel = new JLabel("Select Teams: ");
+      selectTeamsLabel.setBounds(100, 150, 300, 40);
+      teamsPanel.add(selectTeamsLabel);
       for (int i = 0; i < teamBoxes.length; i++) {
-         teamBoxes[i].setBounds(100, 50 + 20 * i, 200, 20);
+         teamBoxes[i].setBounds(100, 180 + 20 * i, 200, 20);
          teamsPanel.add(teamBoxes[i]);
       }
 
@@ -83,6 +124,9 @@ public class CreateTournament {
      | UnsupportedLookAndFeelException e1) {
          e1.printStackTrace();
      }
-     new CreateTournament().createTournamentMenu();
+     CreateTournament ct = new CreateTournament();
+     ct.createTournamentMenu();
+      System.out.println("tournamentName: " + ct.tournamentName);
+      System.out.println("numberUsers: " + ct.numberUsers);
    }
 }
