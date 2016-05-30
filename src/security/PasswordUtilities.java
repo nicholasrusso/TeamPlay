b1eece0c63cfc216004c2b34fdc9da5976724d28
Chauncey Neyman <cjneyman@gmail.com>
2016-05-30 15:56:57 -0700

    512297 - Removed all whitespace

--- src/security/PasswordUtilities.java
+++ src/security/PasswordUtilities.java
@@ -13,41 +13,41 @@ import java.security.NoSuchAlgorithmException;
 
 public class PasswordUtilities {
 
-	public PasswordUtilities() {
-		
-	}
-	public static String getPasswordHash(String password) {
-		Properties props = AppSettings.getInstance();
-		String salt = props.getProperty("salt");
-		MessageDigest md = null;
-		String digest; 
-		
-		byte[] saltbytes;
-		byte[] passbytes;
-		byte[] saltypassbytes = null;
-		try {
-			saltbytes = salt.getBytes("UTF-8");
-			System.out.println(saltbytes);
-			passbytes = password.getBytes("UTF-8");
-			saltypassbytes = new byte[saltbytes.length + passbytes.length];
-			System.arraycopy(passbytes, 0, saltypassbytes, 0, passbytes.length);
-			System.arraycopy(saltbytes, 0, saltypassbytes, passbytes.length, saltbytes.length);
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
-			md = MessageDigest.getInstance("SHA-512");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		
-		if (saltypassbytes != null) {
-			md.update(saltypassbytes);
-		}
-		digest = DatatypeConverter.printHexBinary(md.digest());
-		System.out.println(digest);
-		return digest;		
-	}
+    public PasswordUtilities() {
+        
+    }
+    public static String getPasswordHash(String password) {
+        Properties props = AppSettings.getInstance();
+        String salt = props.getProperty("salt");
+        MessageDigest md = null;
+        String digest; 
+        
+        byte[] saltbytes;
+        byte[] passbytes;
+        byte[] saltypassbytes = null;
+        try {
+            saltbytes = salt.getBytes("UTF-8");
+            System.out.println(saltbytes);
+            passbytes = password.getBytes("UTF-8");
+            saltypassbytes = new byte[saltbytes.length + passbytes.length];
+            System.arraycopy(passbytes, 0, saltypassbytes, 0, passbytes.length);
+            System.arraycopy(saltbytes, 0, saltypassbytes, passbytes.length, saltbytes.length);
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        if (saltypassbytes != null) {
+            md.update(saltypassbytes);
+        }
+        digest = DatatypeConverter.printHexBinary(md.digest());
+        System.out.println(digest);
+        return digest;      
+    }
 
 }
--- src/user/UserSearch.java
+++ src/user/UserSearch.java
@@ -9,88 +9,88 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 
 public class UserSearch {
-	private String usernameQuery;
+    private String usernameQuery;
 
-	public UserSearch(String username) {
-		this.usernameQuery = username;
-	}
-	
-	public void setQuery(String username) {
-		this.usernameQuery = username;
-	}
-	
-	/*
-	 *  Returns an arraylist of User's based on the user's username.
-	 */
-	public ArrayList<User> getUsers() {
-		String getUserSQL = "select username, firstname, lastname, passhash, "
-				          + "email, lastlogin from main.User where username like "
-				          + "'%' || ? || '%' order by username desc";
-		Connection db = DBFactory.getDBConnection();
-		
-		System.out.println("Find Friends Query String: " + this.usernameQuery);
-		
-		String qUsername;
-		String qFirstName;
-		String qLastName;
-		String qEmailAddress;
-		String qPassHash;
-		Long qLastLogin;
-		
-		ArrayList<User> users = new ArrayList<User>();
-		User tempUser;
-    	
-		try {
-			PreparedStatement pstmt = db.prepareStatement(getUserSQL);
-			pstmt.setString(1, this.usernameQuery);			
-			ResultSet rs = pstmt.executeQuery(); 
-		    
-		    while (rs.next()) {
-	    		qUsername = rs.getString("username");
-	    		qFirstName = rs.getString("firstname");
-	    		qLastName = rs.getString("lastname");
-	    		qEmailAddress = rs.getString("email");
-	    		qPassHash = rs.getString("passhash");
-	    		qLastLogin = rs.getLong("lastlogin");
-	    		
-	    		if (notEmpty(qUsername) && notEmpty(qFirstName) &&
-	    		  notEmpty(qLastName) && notEmpty(qEmailAddress)) {
-	    			
-	    			tempUser = new User();
-	    			tempUser.setUsername(qUsername);
-	    			tempUser.setEmail(qEmailAddress);
-	    			tempUser.setFirstName(qFirstName);
-	    			tempUser.setLastName(qFirstName);
-	    			tempUser.setPasswordHash(qPassHash);
-	    			
-	    			users.add(tempUser);
-	    				    			
-	    		}
-	    		qUsername = null;
-	    		qFirstName = null;
-	    		qLastName = null;
-	    		qEmailAddress = null;
-	    		qPassHash = null;
-	    		qLastLogin = null;
-		    }
-		    pstmt.close();
-		    db.close();
+    public UserSearch(String username) {
+        this.usernameQuery = username;
+    }
+    
+    public void setQuery(String username) {
+        this.usernameQuery = username;
+    }
+    
+    /*
+     *  Returns an arraylist of User's based on the user's username.
+     */
+    public ArrayList<User> getUsers() {
+        String getUserSQL = "select username, firstname, lastname, passhash, "
+                          + "email, lastlogin from main.User where username like "
+                          + "'%' || ? || '%' order by username desc";
+        Connection db = DBFactory.getDBConnection();
+        
+        System.out.println("Find Friends Query String: " + this.usernameQuery);
+        
+        String qUsername;
+        String qFirstName;
+        String qLastName;
+        String qEmailAddress;
+        String qPassHash;
+        Long qLastLogin;
+        
+        ArrayList<User> users = new ArrayList<User>();
+        User tempUser;
+        
+        try {
+            PreparedStatement pstmt = db.prepareStatement(getUserSQL);
+            pstmt.setString(1, this.usernameQuery);         
+            ResultSet rs = pstmt.executeQuery(); 
+            
+            while (rs.next()) {
+                qUsername = rs.getString("username");
+                qFirstName = rs.getString("firstname");
+                qLastName = rs.getString("lastname");
+                qEmailAddress = rs.getString("email");
+                qPassHash = rs.getString("passhash");
+                qLastLogin = rs.getLong("lastlogin");
+                
+                if (notEmpty(qUsername) && notEmpty(qFirstName) &&
+                  notEmpty(qLastName) && notEmpty(qEmailAddress)) {
+                    
+                    tempUser = new User();
+                    tempUser.setUsername(qUsername);
+                    tempUser.setEmail(qEmailAddress);
+                    tempUser.setFirstName(qFirstName);
+                    tempUser.setLastName(qFirstName);
+                    tempUser.setPasswordHash(qPassHash);
+                    
+                    users.add(tempUser);
+                                        
+                }
+                qUsername = null;
+                qFirstName = null;
+                qLastName = null;
+                qEmailAddress = null;
+                qPassHash = null;
+                qLastLogin = null;
+            }
+            pstmt.close();
+            db.close();
 
-		} catch (java.sql.SQLException e) {
-			e.printStackTrace();
-			System.out.println("UserQuery: Failed \n" + e.getMessage());
-			System.exit(0);
-		}
-		
-		return users;
-	}
-	
-	private boolean notEmpty(Object obj) {
-		
-		if (obj != null && !obj.toString().isEmpty()) {
-			return true;
-		}
-		
-		return false;
-	}
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+            System.out.println("UserQuery: Failed \n" + e.getMessage());
+            System.exit(0);
+        }
+        
+        return users;
+    }
+    
+    private boolean notEmpty(Object obj) {
+        
+        if (obj != null && !obj.toString().isEmpty()) {
+            return true;
+        }
+        
+        return false;
+    }
 }
