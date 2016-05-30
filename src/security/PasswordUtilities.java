b11c0624b50430f1f47207ed706da6c312d6ffbe
Chauncey Neyman <cjneyman@gmail.com>
2016-05-30 15:56:56 -0700

    512297 - Removed all whitespace

--- src/security/PasswordUtilities.java
+++ src/security/PasswordUtilities.java
@@ -13,37 +13,37 @@ import java.security.NoSuchAlgorithmException;
 
 public class PasswordUtilities {
 
-	public PasswordUtilities() {
-		
-	}
-	public static String getPasswordHash(String password) {
-		Properties props = AppSettings.getInstance();
-		String salt = props.getProperty("salt");
-		MessageDigest md = null;
-		String digest; 
-		
-		byte[] saltbytes;
-		byte[] passbytes;
-		byte[] saltypassbytes = null;
-		try {
-			saltbytes = salt.getBytes("UTF-8");
-			passbytes = password.getBytes("UTF-8");
-			saltypassbytes = new byte[saltbytes.length + passbytes.length];
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
-			md = MessageDigest.getInstance("SHA-512");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		
-		if (saltypassbytes != null) {
-			md.update(saltypassbytes);
-		}
-		digest = DatatypeConverter.printHexBinary(md.digest());
-		return digest;		
-	}
+    public PasswordUtilities() {
+        
+    }
+    public static String getPasswordHash(String password) {
+        Properties props = AppSettings.getInstance();
+        String salt = props.getProperty("salt");
+        MessageDigest md = null;
+        String digest; 
+        
+        byte[] saltbytes;
+        byte[] passbytes;
+        byte[] saltypassbytes = null;
+        try {
+            saltbytes = salt.getBytes("UTF-8");
+            passbytes = password.getBytes("UTF-8");
+            saltypassbytes = new byte[saltbytes.length + passbytes.length];
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        if (saltypassbytes != null) {
+            md.update(saltypassbytes);
+        }
+        digest = DatatypeConverter.printHexBinary(md.digest());
+        return digest;      
+    }
 
 }
