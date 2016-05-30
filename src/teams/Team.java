2159613898aed48af9e35e9c1a9cbb0300bcf288
Charlie Gels <cgels@calpoly.edu>
2016-05-30 10:14:44 -0700

    557970 - smells in Team.java

--- src/teams/Team.java
+++ src/teams/Team.java
@@ -1,20 +1,21 @@
 package teams;
 
-import soccerplayer.SoccerPlayer;
 import java.util.ArrayList;
 import java.util.List;
+
+import soccerplayer.SoccerPlayer;
 
 public class Team {
    private String name = "";
    private String formation = "";
-   public ArrayList<SoccerPlayer> listOfPlayers;
+   public List<SoccerPlayer> listOfPlayers;
 
    // RedMine ID: 512297
    public Team(String name)
    {
       this.name = name;
       this.formation = "4-3-3";
-      listOfPlayers = new ArrayList<SoccerPlayer>();
+      listOfPlayers = new ArrayList<>();
    }
    
    public void addPlayer(SoccerPlayer sp) {
