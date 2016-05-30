aaedd8fb63186d8d9f0bb2004db2da7146c93dd3
Chauncey Neyman <cjneyman@gmail.com>
2016-05-30 15:56:52 -0700

    512297 - Removed all whitespace

--- src/test/TestUserSearch.java
+++ src/test/TestUserSearch.java
@@ -14,38 +14,38 @@ import org.junit.Test;
 
 public class TestUserSearch {
 
-	@Test
-	public void getUsersResultsNotEmpty() {
-		UserSearch searcher = new UserSearch("");
-		ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
-		
-		assertTrue(users.size() > 0);
-	}
-	
-	@Test
-	public void getUsersResultsEmpty() {
-		UserSearch searcher = new UserSearch("DOES_NOT_EXIST");
-		ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
-		
-		assertTrue(users.size() == 0);
-	}
-	
-	@Test
-	public void getUsersResultsAllUsernamesNotEmpty() {
-		UserSearch searcher = new UserSearch("");
-		ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
-	 
-		for (User user : users) {
-			assertNotEquals("", user.getUsername());
-			System.out.println(user.getUsername());
-		}
-	}
-	
-	@Test
-	public void getUsersResultsFindTestUser() {
-		UserSearch searcher = new UserSearch("testuser1");
-		ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
-		
-		assertTrue(users.size() == 1);
-	}
+    @Test
+    public void getUsersResultsNotEmpty() {
+        UserSearch searcher = new UserSearch("");
+        ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
+        
+        assertTrue(users.size() > 0);
+    }
+    
+    @Test
+    public void getUsersResultsEmpty() {
+        UserSearch searcher = new UserSearch("DOES_NOT_EXIST");
+        ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
+        
+        assertTrue(users.size() == 0);
+    }
+    
+    @Test
+    public void getUsersResultsAllUsernamesNotEmpty() {
+        UserSearch searcher = new UserSearch("");
+        ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
+     
+        for (User user : users) {
+            assertNotEquals("", user.getUsername());
+            System.out.println(user.getUsername());
+        }
+    }
+    
+    @Test
+    public void getUsersResultsFindTestUser() {
+        UserSearch searcher = new UserSearch("testuser1");
+        ArrayList<User> users = (ArrayList<User>) searcher.getUsers();
+        
+        assertTrue(users.size() == 1);
+    }
 }
