8699de9ff57c4e3730375b726bc8d0e1a3de8817
Chauncey Neyman <cjneyman@gmail.com>
2016-05-31 13:14:58 -0700

    512297 - Put name in test suite comments

--- src/test/NeymanTestSuite.java
+++ src/test/NeymanTestSuite.java
@@ -1,5 +1,10 @@
 package test;
 
+/*
+ * Test Type: Test Suite of unit tests and integration tests.
+ * Author: Chauncey Neyman
+ */
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
