bacfbb0f83d0cdf9661b1d64dc2df56029f5c5d7
nicholasrusso <nicholasrusso@users.noreply.github.com>
2016-05-31 14:02:01 -0700

    557972 fixing code smells

---
+++ src/test/SprowlsTestSuite.java
@@ -0,0 +1,16 @@
+package test;
+
+/*
+ * Test Type: Test Suite of unit tests and integration tests.
+ * Author: Spencer Sprowls
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestDefender.class, TestForward.class, TestGoalKeeper.class})
+public class SprowlsTestSuite {
+
+}
